5e7fa2037c454198bf0156376b9ac7697641c94e
nour-ha123 <hanafi.nour11@gmail.com>
2026-03-10 21:59:40 +0100

    modifminiprojet

---
+++ script.js
@@ -0,0 +1,153 @@
+var produits = JSON.parse(localStorage.getItem("produits")) || [];
+var tbody = document.getElementById("produits-body");
+var input = document.getElementById("input");
+var prix = document.getElementById("prix");
+var imageInput = document.getElementById("imageInput");
+var button = document.getElementById("button");
+var compteur = document.querySelector("h1");
+
+function afficheProduits() {
+    tbody.innerHTML = "";
+
+    produits.forEach((el, index) => {
+        var tr = document.createElement("tr");
+        var tdId = document.createElement("td");
+        var tdImage = document.createElement("td");
+        var tdAction = document.createElement("td");
+        var tdPrix = document.createElement("td");
+        var tdVendu = document.createElement("td");
+        var tdDate = document.createElement("td");
+        var tdActions = document.createElement("td");
+
+        tdId.textContent = el.id.slice(0, 3);
+        tdAction.textContent = el.value;
+        tdPrix.textContent = el.prix + " DT";
+        tdVendu.textContent = el.vendu ? "Oui" : "Non";
+        tdDate.textContent = el.date;
+
+        if (el.image) {
+            var img = document.createElement("img");
+            img.src = el.image;
+            img.style.width = "60px";
+            img.style.borderRadius = "5px";
+            tdImage.appendChild(img);
+        }
+
+        var imgEdit = document.createElement("img");
+        imgEdit.src = "/edit.png";
+        imgEdit.style.width = "25px";
+        imgEdit.style.marginRight = "10px";
+        imgEdit.style.cursor = "pointer";
+        imgEdit.onclick = () => { 
+            var inputEdit = document.createElement("input"); 
+            inputEdit.value = el.value;
+            var save = document.createElement("button"); 
+            save.textContent = "save";
+            tdActions.append(inputEdit, save); 
+            save.onclick = () => { 
+                imgEditFunc(el.id, inputEdit.value);
+            }; 
+        }; 
+
+        var imgSupp = document.createElement("img");
+        imgSupp.src = "/trash.png";
+        imgSupp.style.width = "25px";
+        imgSupp.style.cursor = "pointer";
+        imgSupp.onclick = (event) => {
+            event.stopPropagation();
+            supprimerProduit(index);
+
+        };
+
+        tdActions.append(imgEdit, imgSupp);
+
+        if (el.vendu === true) {
+            tdVendu.style.textDecoration = "line-through";
+            tdVendu.style.color = "red";
+        }
+
+        tdVendu.style.cursor = "pointer";
+        tdVendu.onclick = () => {
+            produitVendu(el);
+        };
+
+        tr.append(tdId, tdAction, tdPrix, tdVendu, tdDate, tdImage, tdActions);
+        tbody.appendChild(tr);
+    });
+
+    compteurProduits();
+}
+
+button.onclick = function () {
+
+    var file = imageInput.files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function () {
+        var produit = {
+            id: crypto.randomUUID(),
+            value: input.value,
+            prix: prix.value,
+            image: file ? reader.result : null,
+            date: new Date().toLocaleString(),
+            vendu: false,
+        };
+
+        produits.push(produit);
+        localStorage.setItem("produits", JSON.stringify(produits));
+
+        input.value = "";
+        prix.value = "";
+        imageInput.value = "";
+
+        afficheProduits();
+    };
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        reader.onloadend();
+    }
+}
+
+function produitVendu(el) {
+    el.vendu = !el.vendu;
+    localStorage.setItem("produits", JSON.stringify(produits));
+    afficheProduits();
+}
+
+function supprimerProduit(index) {
+    produits.splice(index, 1);
+    localStorage.setItem("produits", JSON.stringify(produits));
+    afficheProduits();
+}
+
+function compteurProduits() {
+    if (compteur) {
+        compteur.textContent = "On a : " + produits.length + " produits";
+    }
+}
+
+function imgEditFunc(id, value) { 
+    produits = produits.map((el) => { 
+        if (el.id === id) { 
+            return { 
+                id: el.id, 
+                value: value, 
+                prix: el.prix, 
+                image: el.image, 
+                date: el.date, 
+                vendu: el.vendu, 
+            }; 
+        } 
+        return el; 
+    }); 
+    localStorage.setItem("produits", JSON.stringify(produits)); 
+    afficheProduits(); 
+} 
+
+
+afficheProduits();
+
+
+
